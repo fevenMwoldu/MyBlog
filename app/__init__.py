ede936e8cabce2ce4f50e435a30b2bb46ff39862
feven <feven.m.woldu@gmail.com>
2019-07-05 10:51:19 +0300

    modified my __init__.py file with import from config

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,24 @@ from flask import Flask
 from flask import Blueprint
 
 from flask_bootstrap import Bootstrap
+from .config import DevConfig
+
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 bootstrap = Bootstrap()
 
-# Initializing application
-app = Flask(__name__) 
 
 def create_app():
 
     bootstrap.init_app(app)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
     # Registering the blueprint
 
