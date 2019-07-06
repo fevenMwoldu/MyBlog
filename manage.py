31d71ea8a5ff96acd2ad9915e55b619320102c00
feven <feven.m.woldu@gmail.com>
2019-07-06 08:49:31 +0300

    modified the mange.py file

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 from app import create_app
 from flask_script import Manager,Server
 
+# Creating app instance
+app = create_app('development')
 
-app = create_app()
 manager = Manager(app)
 manager.add_command('server',Server)
+
 if __name__ == '__main__':
     manager.run()
