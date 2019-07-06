e575c38ffc5b90167701620f931a0ab5cf3ac0dd
feven <feven.m.woldu@gmail.com>
2019-07-06 21:16:44 +0300

    modified this file

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,13 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
     bootstrap.init_app(app)
     db.init_app(app)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+
 
     # Registering the blueprint
     from .main import main as main_blueprint
