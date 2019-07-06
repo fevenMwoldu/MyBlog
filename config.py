5f293e539d1343067da48de8c6bd9f725546a4b1
feven <feven.m.woldu@gmail.com>
2019-07-06 21:14:58 +0300

    included the databse URI

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://feven:123@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
 class ProdConfig(Config):
