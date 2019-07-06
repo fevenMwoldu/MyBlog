3315a3fc12edb43edf483c13de699a091d66c1e0
feven <feven.m.woldu@gmail.com>
2019-07-06 21:26:32 +0300

    modified this file to check my tables functions  with the flask script shell

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
+from app.models import User,Blog,Post,Comment
 
 # Creating app instance
 app = create_app('development')
@@ -10,6 +11,11 @@ manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db,
+                User=User, Blog=Blog, Post=Post, Comment=Comment)
 
 if __name__ == '__main__':
     manager.run()
