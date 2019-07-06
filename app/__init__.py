c7ecd733807accdf82884ccf4d896adbd3ef3e12
feven <feven.m.woldu@gmail.com>
2019-07-06 08:51:31 +0300

    imported the flask_sqlalchemy and instantiated the db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,30 @@
 from flask import Flask
 from flask import Blueprint
-
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_name):
+
+    app = Flask(__name__)
 
     bootstrap.init_app(app)
+    db.init_app(app)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
     # Registering the blueprint
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
     return app
